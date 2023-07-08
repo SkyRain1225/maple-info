2ef49ba8c3e9acd831cd0f5fded7aa9e8158f486
SkyRain1225 <rudwns9286@gmail.com>
2023-07-09 04:51:44 +0900

    ✨ 나눔바른고딕 폰트 추가

--- src/styles/GlobalStyled.ts
+++ src/styles/GlobalStyled.ts
@@ -1,9 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyled = createGlobalStyle`
+
+  @font-face {
+    font-family: 'NanumBarunGothic';
+    src: url('~/fonts/NanumBarunGothic.eot');
+    font-weight: normal;
+    
+  }
+
+  :root {
+    --font-nanum: 'NanumBarunGothic';
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: var(--font-nanum);
   }
 `;
